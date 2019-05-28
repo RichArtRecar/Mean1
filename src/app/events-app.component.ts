32b7b490b8e57d5d2730a268d38debd95e86bc62
Rich Recar <richard_recar@outlook.com>
2019-05-28 15:47:21 -0400

    "Cleared out app"

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'events-app',
+  template: '<h1>hELLO bITCH<h1>',
+})
+export class EventsAppComponent {
+  title = 'meanStack1';
+}
