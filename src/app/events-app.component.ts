fd556a705493201d8529f8e5bb8085c22bc8dbf3
Rich Recar <richard_recar@outlook.com>
2019-05-28 17:46:47 -0400

    "static files"

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'events-app',
-  template: '<h1>hELLO bITCH<h1>',
+  template: `
+            <h1>hELLO bITCH<h1>
+            <img src="/assets/images/basic-shield.png"/>
+  `
 })
 export class EventsAppComponent {
   title = 'meanStack1';
